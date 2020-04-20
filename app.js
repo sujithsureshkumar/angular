a3a845a0172d7d8facd34a7d7729aad5d2fb40d4
sujithsureshkumar <sujithsureshkumar081@gmail.com>
2020-04-20 16:26:18 +0530

    my first assignment

---
+++ app.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('Lunch', [])
+.controller('lunchChecker', lunchChecker);
+
+lunchChecker.$inject = ['$scope'];
+function lunchChecker($scope) {
+  
+
+
+  $scope.Hello = function () {
+    var lunchMenu  = document.getElementById('lunch-menu').value;
+    if(lunchMenu===""){
+      $scope.msg = "PLease enter data first";
+      console.log("En");
+    }
+    else{
+    var result;
+      result = lunchMenu.split(","); 
+      console.log(result);
+      if(result.length<=3){
+      console.log("Enjoy");
+      $scope.msg = "Enjoy";
+
+}
+else{
+  console.log("Too much");
+  $scope.msg = "Too much";
+}
+    }
+  };
+}
+
+
+})();
